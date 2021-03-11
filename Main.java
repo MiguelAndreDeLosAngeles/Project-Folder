955d366fc2a0df3f812d770e6035ae58d532c612
MiguelAndreDeLosAngeles <79899064+MiguelAndreDeLosAngeles@users.noreply.github.com>
2021-03-11 12:09:35 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class assignment004
+{
+public static void main(String[] args0{
+System.out.println("Miguel Andre De Los Angeles");
+System.out.println("Lanz Positos");
+System.out.println("Karl Pescuela");
+System.out.println("Vincent Beronia");
+System.out.println("John Floyd Alvarez");
+}
+}
